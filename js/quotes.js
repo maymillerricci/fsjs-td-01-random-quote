cbf1a9ac18bf7974c6a6a5a80f62896ccf797fe4
May Miller-Ricci <maymillerricci@gmail.com>
2016-07-28 19:51:58 -0700

    add quotes array

---
+++ js/quotes.js
@@ -0,0 +1,73 @@
+quotes = [
+  { 
+  	quote: "They may take our lives, but they'll never take our freedom!", 
+  	source: "William Wallace", 
+  	citation: "Braveheart", 
+  	year: 1995
+  },
+  { 
+  	quote: "I don't know who you are. I don't know what you want. If you are looking for ransom, I can tell you I don't have money. But what I do have are a very particular set of skills, skills I have acquired over a very long career. Skills that make me a nightmare for people like you.", 
+  	source: "Bryan Mills", 
+  	citation: "Taken", 
+  	year: 2008
+  },
+  { 
+  	quote: "After all, tomorrow is another day!", 
+  	source: "Scarlett O'Hara", 
+  	citation: "Gone With the Wind", 
+  	year: 1939
+  },
+  { 
+  	quote: "I'm not bad. I'm just drawn that way.", 
+  	source: "Jessica Rabbit", 
+  	citation: "Who Framed Roger Rabbit", 
+  	year: 1988
+  },
+  { 
+  	quote: "I'm the king of the world!", 
+  	source: "Jack Dawson", 
+  	citation: "Titanic", 
+  	year: 1997
+  },
+  { 
+  	quote: "Roads? Where we're going we don't need roads.", 
+  	source: "Dr. Emmett Brown", 
+  	citation: "Back To The Future", 
+  	year: 1985
+  },
+  { 
+  	quote: "Show me the money!", 
+  	source: "Jerry Maguire,", 
+  	citation: "Jerry Maguire,", 
+  	year: 1996
+  },
+  { 
+    quote: "Love means never having to say you're sorry.", 
+    source: "Love Story", 
+    year: 1970
+  },
+  { 
+  	quote: "There's no crying in baseball!", 
+  	source: "Jimmy Dugan", 
+  	citation: "A League of Their Own", 
+  	year: 1992
+  },
+  { 
+  	quote: "To infinity and beyond!", 
+  	source: "Buzz Lightyear", 
+  	citation: "Toy Story", 
+  	year: 1995
+  },
+  { 
+    quote: "Hello. My name is Inigo Montoya. You killed my father. Prepare to die.", 
+    source: "Inigo Montoya.", 
+    citation: "The Princess Bride", 
+    year: 1987
+  },
+  { 
+    quote: "Of all the gin joints in all the towns in all the world, she walks into mine.", 
+    source: "Rick Blaine", 
+    citation: "Casablanca", 
+    year: 1942
+  }
+];
