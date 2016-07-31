5147797df89d7e63c2b63e1799bef648b0c12f7e
May Miller-Ricci <maymillerricci@gmail.com>
2016-07-31 10:42:50 -0700

    add JS to get page working with displaying random quote

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// returns a random integer from 0 to topNum
+function getRandomNumber(topNum) {
+	return Math.floor(Math.random() * topNum);
+}
+
+// returns a quote object at a randomly chosen index from the quotes array
+function getRandomQuote(quotes) {
+	randomNum = getRandomNumber(quotes.length);
+	return quotes[randomNum];
+}
+
+// replaces the html on the page with the html string of the random quote
+function printQuote() {
+  quote = getRandomQuote(quotes);
+  htmlString = "<p class='quote'>" + quote.quote + "</p><p class='source'>" + quote.source + "<span class='citation'>" + quote.citation + "</span> <span class='year'>" + quote.year + "</span> </p>";
+  document.getElementById('quote-box').innerHTML = htmlString;
+}
+
+printQuote();
