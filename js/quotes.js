4aabe8f8c15c06a94045f6842606be5074600de0
May Miller-Ricci <maymillerricci@gmail.com>
2016-07-31 19:50:59 -0700

    add genre, favorite properties to quotes

--- js/quotes.js
+++ js/quotes.js
@@ -3,90 +3,110 @@ var quotes = [
   	quote: "They may take our lives, but they'll never take our freedom!", 
   	source: "William Wallace", 
   	citation: "Braveheart", 
-  	year: 1995
+  	year: 1995,
+    genre: "action"
   },
   {
     quote: "I don't know who you are. I don't know what you want. If you are looking for ransom, I can tell you I don't have money. But what I do have are a very particular set of skills, skills I have acquired over a very long career. Skills that make me a nightmare for people like you.", 
     source: "Bryan Mills", 
     citation: "Taken", 
-    year: 2008
+    year: 2008,
+    genre: "action"
   },
   { 
   	quote: "After all, tomorrow is another day!", 
   	source: "Scarlett O'Hara", 
   	citation: "Gone With the Wind", 
-  	year: 1939
+  	year: 1939,
+    genre: "drama"
   },
   { 
   	quote: "I'm not bad. I'm just drawn that way.", 
   	source: "Jessica Rabbit", 
-  	citation: "Who Framed Roger Rabbit"
+  	citation: "Who Framed Roger Rabbit",
+    genre: "comedy"
   },
   { 
   	quote: "I'm the king of the world!", 
   	source: "Jack Dawson", 
   	citation: "Titanic", 
-  	year: 1997
+  	year: 1997,
+    genre: "drama"
   },
   { 
   	quote: "Roads? Where we're going we don't need roads.", 
   	source: "Dr. Emmett Brown", 
   	citation: "Back To The Future", 
-  	year: 1985
+  	year: 1985,
+    genre: "comedy",
+    favorite: true
   },
   { 
   	quote: "Show me the money!", 
   	source: "Jerry Maguire", 
   	citation: "Jerry Maguire", 
-  	year: 1996
+  	year: 1996,
+    genre: "drama"
   },
   { 
     quote: "Love means never having to say you're sorry.", 
-    source: "Love Story"
+    source: "Love Story",
+    genre: "drama"
   },
   { 
   	quote: "There's no crying in baseball!", 
   	source: "Jimmy Dugan", 
   	citation: "A League of Their Own", 
-  	year: 1992
+  	year: 1992,
+    genre: "drama"
   },
   { 
   	quote: "To infinity and beyond!", 
   	source: "Buzz Lightyear", 
   	citation: "Toy Story", 
-  	year: 1995
+  	year: 1995,
+    genre: "comedy"
   },
   { 
     quote: "Hello. My name is Inigo Montoya. You killed my father. Prepare to die.", 
     source: "The Princess Bride", 
-    year: 1987
+    year: 1987,
+    genre: "comedy",
+    favorite: true
   },
   { 
     quote: "Of all the gin joints in all the towns in all the world, she walks into mine.", 
     source: "Rick Blaine", 
     citation: "Casablanca", 
-    year: 1942
+    year: 1942,
+    genre: "drama",
+    favorite: true
   },
   { 
     quote: "There's no place like home.", 
     source: "Dorothy Gale", 
     citation: "Wizard of Oz", 
-    year: 1939
+    year: 1939,
+    genre: "drama",
+    favorite: true
   },
   { 
     quote: "I see dead people.", 
     source: "Cole Sear", 
     citation: "The Sixth Sense", 
-    year: 1999
+    year: 1999,
+    genre: "drama"
   },
   { 
     quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.", 
     source: "John Keating", 
-    citation: "Dead Poets Society"
+    citation: "Dead Poets Society",
+    genre: "drama"
   },
   {  
     quote: "Nobody puts Baby in a corner.", 
     source: "Dirty Dancing", 
-    year: 1987
+    year: 1987,
+    genre: "drama"
   }
 ];
